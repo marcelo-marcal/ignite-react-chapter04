d10ea695d1972e09fbc6b1df2f5e2e4dc8e1fe50
Marcelo Marçal <marcelo.a.marcal@hotmail.com>
2022-05-02 18:57:06 -0300

    Home2

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,7 +1,41 @@
+import { Box, Flex, SimpleGrid, Text } from "@chakra-ui/react";
 import { Header } from "../components/Header";
+import { Sidebar } from "../components/Sidebar";
 
 export default function sisLab() {
   return (
-    <Header />    
+    <Flex
+      direction="column"
+      height="100vh"
+    >
+      <Header />
+      <Flex
+        width="100%"
+        marginY="6"
+        maxWidth={1480}
+        marginX="auto"
+        paddingX="6"
+      >
+        <Sidebar />
+        <SimpleGrid flex="1" gap="4" minChildWidth="320px" alignItems="flex-start">
+          <Box
+            padding="8"
+            backgroundColor="gray.800"
+            borderRadius={8}
+            // paddingBottom="4"
+          >
+            <Text fontSize="lg" marginBottom="4" >Inscritos da Semana</Text>
+          </Box>
+          <Box
+            padding="8"
+            backgroundColor="gray.800"
+            borderRadius={8}
+            // paddingBottom="4"
+          >
+            <Text fontSize="lg" marginBottom="4" >Taxa de Abertura</Text>
+          </Box>
+        </SimpleGrid>
+      </Flex>
+    </Flex>
   )
 }
